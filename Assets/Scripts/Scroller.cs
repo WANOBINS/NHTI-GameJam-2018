60baa84148f0294df2265858eb341403f8271726
Jack Becker <jigglypuff100@gmail.com>
2018-11-17 14:09:35 -0500

    changed from vertical to horizontal scroller

--- Assets/Scripts/Scroller.cs
+++ Assets/Scripts/Scroller.cs
@@ -13,14 +13,14 @@ public class Scroller : MonoBehaviour {
     void Start ()
     {
         savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset("_MainTex");
-        tileSize = transform.lossyScale.y;
+        tileSize = transform.lossyScale.x;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        float scroll_Y = Mathf.Repeat(Time.time * scrollSpeed, tileSize);
-        Vector2 offset = new Vector2(savedOffset.x, scroll_Y);
+        float scroll_X = Mathf.Repeat(Time.time * scrollSpeed, tileSize);
+        Vector2 offset = new Vector2( scroll_X, savedOffset.y);
         GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
     }
 }
