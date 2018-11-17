faf05a1241e4ef909d6dbbb5e25a167d0cd17906
Jack Becker <jigglypuff100@gmail.com>
2018-11-17 03:52:35 -0500

    started turret AI

---
+++ Assets/Scripts/Turret AI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAI : MonoBehaviour
+{
+    public bool isRed = false;
+    [SerializeField] private float range = 1f;
+  
+	// Use this for initialization
+	void Start ()
+    {
+        isRed = (Random.Range(0, 1) == 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       if( Vector3.Distance(gameObject.transform.position, GameObject.FindGameObjectWithTag("RedPlayer").transform.position ) <= range)
+        {
+
+        }
+        else if (Vector3.Distance(gameObject.transform.position, GameObject.FindGameObjectWithTag("BluePlayer").transform.position) <= range)
+        {
+
+        }
+    }
+}
