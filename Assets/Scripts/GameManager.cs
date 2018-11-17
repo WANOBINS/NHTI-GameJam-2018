d64461211e24e6b03bcc1684ef1f53f1be7c1e29
DaemonUmbra <daemon.umbra@gmail.com>
2018-11-16 23:03:58 -0500

    Updated WBase, added my test scene and script to make it work

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
-
+    public bool DebugQuit = true;
 	// Use this for initialization
 	void Start () {
-		
+        DontDestroyOnLoad(transform);
+        SceneManager.LoadScene("Colby's Scene", LoadSceneMode.Additive); //Allows me to work in my scene and have it be merged into the main scene at runtime
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (DebugQuit && Input.GetKey(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+                EditorApplication.OpenURL("https://google.com");
+#else
+            Application.Quit();
+#endif
+        }
 	}
 }
