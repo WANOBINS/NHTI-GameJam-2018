e5d99c768ea9d8fa8f96c7acaaf69a7a4b298f65
Jack Becker <jigglypuff100@gmail.com>
2018-11-16 22:27:46 -0500

    added test assets and scroller script

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour {
+
+    public float scrollSpeed;
+    public float tileSize;
+
+    private Vector3 startPos;
+
+    // Use this for initialization
+    void Start ()
+    {
+        startPos = transform.position;
+        tileSize = transform.lossyScale.y;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, tileSize);
+        transform.position = startPos + Vector3.forward * newPos;
+    }
+}
