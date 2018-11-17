e55dcbb720c50f37c5e4095fd3774292e704f3b0
Jack Becker <jigglypuff100@gmail.com>
2018-11-17 14:03:45 -0500

    turret tracking

--- Assets/Scripts/Turret AI.cs
+++ Assets/Scripts/Turret AI.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 public class TurretAI : MonoBehaviour
 {
     public bool isRed = false;
+    public float speed = 1f;
+    
     [SerializeField] private float range = 1f;
-  
+
+    Quaternion aimRotation;
+    Vector3 lastTargetPos = Vector3.zero;
+    GameObject targetPlayer = null;
+    
 	// Use this for initialization
 	void Start ()
     {
         isRed = (Random.Range(0, 1) == 0);
 	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-       if( Vector3.Distance(gameObject.transform.position, GameObject.FindGameObjectWithTag("RedPlayer").transform.position ) <= range)
-        {
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(gameObject.transform.position, GameObject.FindGameObjectWithTag("RedPlayer").transform.position) <= range)
+        {
+            targetPlayer = GameObject.FindGameObjectWithTag("RedPlayer");
         }
         else if (Vector3.Distance(gameObject.transform.position, GameObject.FindGameObjectWithTag("BluePlayer").transform.position) <= range)
         {
+            targetPlayer = GameObject.FindGameObjectWithTag("BluePlayer");
+        }
 
+        if (targetPlayer)
+        {
+            if (lastTargetPos != targetPlayer.transform.position)
+            {
+                lastTargetPos = targetPlayer.transform.position;
+                aimRotation = Quaternion.LookRotation(lastTargetPos - transform.position);
+            }
+
+            if (transform.rotation != aimRotation)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, aimRotation, speed * Time.deltaTime);
+            }
         }
     }
 }
