a0570ccc3d143a484a917f45b42fe2f4bd8aecae
GuyManTheDude <33268653+GuyManTheDude@users.noreply.github.com>
2018-11-18 12:25:17 -0500

    More Polish

--- Assets/Scripts/FighterAI.cs
+++ Assets/Scripts/FighterAI.cs
@@ -11,6 +11,9 @@ public class FighterAI : MonoBehaviour
     public float frequency = 20f;
     public float magnitude = 5f;
     public float health = 3;
+    public Material HitFlash;
+    public Material CurMat;
+    public Material MyMat;
 
     public bool isRed = false;
     private Vector3 pos;
@@ -28,19 +31,32 @@ public class FighterAI : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(90f, 0, 90f);
 
+        CurMat = gameObject.GetComponent<MeshRenderer>().material;
+
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         pos += transform.up * Time.deltaTime * MoveSpeed;
-        transform.position = pos + transform.right * Mathf.Sin(Time.time * frequency) * magnitude;
+        transform.position = pos + transform.right * Mathf.Sin(Time.time * frequency) * magnitude;        
+        if(CurMat == HitFlash)
+        {
+            Invoke("ResetMat", 0.2f);
+        }
 	}
+
+    void ResetMat()
+    {
+        CurMat = MyMat;
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "P1Bullet" && isRed)
         {
+            CurMat = HitFlash;
             health--;
             Destroy(other.gameObject);
             if (health <= 0)
@@ -52,6 +68,7 @@ public class FighterAI : MonoBehaviour
         }
         else if (other.gameObject.tag == "P2Bullet" && !isRed)
         {
+            CurMat = HitFlash;
             health--;
             Destroy(other.gameObject);
             if (health <= 0)
